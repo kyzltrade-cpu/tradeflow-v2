8e79c83f883d26abbcfdde8f2e322f3171caa65b
Kyle Cheung <kylecheung@Kyles-MacBook-Air-3.local>
2026-09-22 21:14:06 +0800

    bypass auth for demo

--- src/middleware.ts
+++ src/middleware.ts
@@ -25,22 +25,7 @@ export async function middleware(request: NextRequest) {
     }
   );
 
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
-
-  if (
-    !user &&
-    !request.nextUrl.pathname.startsWith('/login') &&
-    !request.nextUrl.pathname.startsWith('/signup') &&
-    !request.nextUrl.pathname.startsWith('/auth') &&
-    request.nextUrl.pathname.startsWith('/admin')
-  ) {
-    const url = request.nextUrl.clone();
-    url.pathname = '/login';
-    return NextResponse.redirect(url);
-  }
-
+  // Auth bypassed for demo
   return supabaseResponse;
 }
 
